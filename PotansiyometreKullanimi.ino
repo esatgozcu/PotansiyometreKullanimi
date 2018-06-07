0d898d9516ed36ce3791d8447f97cf4ba89470c1
esatgozcu <35576161+esatgozcu@users.noreply.github.com>
2018-06-07 05:51:13 +0300

    Add files via upload

---
+++ PotansiyometreKullanimi.ino
@@ -0,0 +1,26 @@
+const int analogPin = A0;
+float potansDeger = 0;
+float gerilimDeger = 0;
+
+void setup() {
+  
+  Serial.begin(9600);         
+}
+
+void loop() {
+
+ potansDeger = analogRead(analogPin);
+
+ Serial.print("Potansiyometre Degeri : ");
+ Serial.println(potansDeger);
+
+ gerilimDeger = (potansDeger/1023.0)*5.0;
+ 
+ Serial.print("Gerilim Degeri : ");
+ Serial.println(gerilimDeger);
+
+ delay(500);
+}
+
+
+
